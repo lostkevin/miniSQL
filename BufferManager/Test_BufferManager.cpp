63d617bf2333aaf8c2c55401d2b025407c19ef43
zcc_zju <3170102488@zju.edu.cn>
2019-06-20 21:30:25 +0800

    buffer_test修改了下结尾加了'\0'，链接器修改了下

--- BufferManager/Test_BufferManager.cpp
+++ BufferManager/Test_BufferManager.cpp
@@ -10,10 +10,17 @@ int main () {
 	BYTE array[400];	
 	for (int i = 1; i <= 10000; i++) {
 		IndexInfo tmp = bmgr.createBlock (fileName, 400);
-		for (int j = 0; j < 400; j++) {
-			array[j] = i % 256;
+		for (int j = 0; j < 399; j++) {
+			array[j] = j % 10 + '0';
 		}
+		array[399] = '\0';
+		char t[100];
+		bmgr.readRawData(fileName, tmp, t);
+		cout << "1 " << t <<endl;
 		bmgr.WriteRawData (fileName, tmp, array);
+		cout << "2 " << array <<endl;
+		bmgr.readRawData(fileName, tmp, t);
+		cout << "3 " << t << endl;
 		if (rand () % 2)bmgr.erase (fileName, tmp);
 	}
 
--- IndexManager/Test_IndexMgr.cpp
+++ IndexManager/Test_IndexMgr.cpp
@@ -10,7 +10,7 @@ int main () {
 	
 	//1.实例化buffer
 	BufferManager bMgr;
-	string fileName = "DB.bin";
+	string fileName = "DB1.bin";
 	//2.实例化indexmanager
 	IndexManager iMgr (fileName, bMgr);
 	//3.设置表的类型，可以检查返回值是否为true
@@ -18,7 +18,7 @@ int main () {
 	
 	//使用buffer存入数据时，将返回一个indexinfo，对于主键的自动建立，必须马上把这个indexinfo加到indexmanager里
 	//例如
-	IndexInfo info = bMgr.createBlock ("table.dat");
+	IndexInfo info = bMgr.createBlock ("table.dat" , 400);
 	iMgr.insert (3170103618, info);
 	//当主键已经建立，给unique键手动建立索引时，使用find查找需要的indexinfo
 	//假设以primary key ——code为key的 index已经建好，文件名是p.index
